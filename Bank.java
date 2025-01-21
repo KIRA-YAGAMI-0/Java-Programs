e8f76fd1f0f548890b3a12fb7a280da3fd69f18d
KIRA-YAGAMI-0 <pm3216099@gmail.com>
2025-01-21 18:45:25 +0545

    Added initial programs which i made by learning java as a beginner

---
+++ Bank.java
@@ -0,0 +1,68 @@
+import java.util.Scanner;
+
+class BankAccount {
+    String accountNumber;
+    String accountHolderName;
+    double balance;
+double amt;
+    public BankAccount(String ac, String ach, double b) {
+        accountNumber = ac;
+        accountHolderName = ach;
+        balance = b;
+    }
+
+    public void deposite(double amount) {
+        System.out.println("Deposite Amount: "+amount);
+        amount +=balance;
+        System.out.println("Updated Balance: "+amount);
+        amt=amount;
+    }
+
+    public void withdraw(double amount) {
+        System.out.println("Withdraw Amount: "+amount);
+        amt -=amount;
+        System.out.println("Updated Balance: "+amt);
+    }
+
+    public void displayAccountDetails() {
+System.out.println("Account Details:");
+System.out.println("Account Number: "+accountNumber);
+System.out.println("Account Holder: "+accountHolderName);
+System.out.println("Balance: "+amt);
+    }
+
+}
+
+public class Bank {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter account number: ");
+        String accnum = sc.next();
+
+        System.out.print("Enter account holder name: ");
+        String accholder = sc.next();
+
+        System.out.print("Enter initial balance: ");
+        double initbal = sc.nextDouble();
+        System.out.println();
+        System.out.println();
+        
+        BankAccount obj = new BankAccount(accnum, accholder,initbal);
+
+        System.out.print("Please ! Enter the Deposite Amount: ");
+        double deposite =sc.nextDouble();
+        obj.deposite(deposite);
+        System.out.println();
+
+        System.out.print("Please ! Enter the withdraw Amount: ");
+        double with =sc.nextDouble();
+        obj.withdraw(with);
+        System.out.println();
+
+        obj.displayAccountDetails();
+        System.out.println();
+        sc.close();
+    }
+}
---
+++ Bankkkk.java
@@ -0,0 +1,94 @@
+import java.util.Scanner;
+
+class BankAccount {
+    String accountNumber;
+    String accountHolderName;
+    double balance;
+
+    public BankAccount(String accNum, String ach, double bal) {
+        accountNumber = accNum;
+        accountHolderName = ach;
+        balance = bal;
+    }
+
+    public void displayAccountDetails() {
+        // System.out.println("Sender Account Details:");
+        System.out.println("Account Number:" + accountNumber);
+        System.out.println("Account Holder:" + accountHolderName);
+        System.out.println("Balance:" + balance);
+
+    }
+
+    public void deposit(double amount) {
+        balance += amount;
+        // System.out.println("The deposited amount +initial balance is:" + balance);
+    }
+
+    public void withdraw(double amount) {
+        if (balance > 0 && balance >= amount) {
+            balance -= amount;
+            // System.out.println("The initial balance - withdraw amount  is:" + balance);
+        } else {
+            System.out.println("Insufficient Balance:" + balance);
+        }
+    }
+
+}
+
+class Transaction {
+    public void transferFunds(BankAccount sender, BankAccount receiver, double amount) {
+        if (sender.balance >= amount) {
+            sender.withdraw(amount);
+            receiver.deposit(amount);
+            System.out.println("Transfering "+amount+" from " +sender.accountHolderName+ " to "+ receiver.accountHolderName+"...");
+System.out.println("Transfer successful!");
+        } else {
+            System.out.println("Insufficient balance for transfer.");
+        }
+    }
+}
+
+public class Bankk {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        System.out.println("For sender:");
+        System.out.print("Enter the account number:");
+        String acc =sc.next();
+        System.out.print("Enter the account holder name:");
+        String acch=sc.next();
+        System.out.print("Enter the initial balance:");
+        double bl=sc.nextDouble();
+
+        BankAccount send = new BankAccount(acc, acch,bl);
+        System.out.println("For receiver:");
+        System.out.print("Enter the account number:");
+        String acc1 =sc.next();
+        System.out.print("Enter the account holder name:");
+        String acch1=sc.next();
+        System.out.print("Enter the initial balance:");
+        double bl1=sc.nextDouble();
+        BankAccount rec = new BankAccount(acc1, acch1, bl1);
+
+        System.out.println("Sender Account Details:");
+        send.displayAccountDetails();
+
+        System.out.println();
+
+        System.out.println("Receiver Account Details:");
+        rec.displayAccountDetails();
+
+        System.out.println("Enter the amount you want to transfer:");
+        double trana =sc.nextDouble();
+
+        Transaction trans = new Transaction();
+        trans.transferFunds(send, rec, trana);
+
+
+        System.out.println("\nUpdated Sender Account Details:");
+        send.displayAccountDetails();
+
+        System.out.println("\nUpdated Receiver Account Details:");
+        rec.displayAccountDetails();
+    }
+}
