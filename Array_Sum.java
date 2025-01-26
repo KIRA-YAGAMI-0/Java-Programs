30819401be6dd7b586034ef0de92dad8739a7d9d
KIRA-YAGAMI-0 <pm3216099@gmail.com>
2025-01-26 08:04:28 +0545

    Combined java programs of finding specific value in array ,sum of array and to also display their index number.

--- Array_Sum.java
+++ Array_Sum.java
@@ -1,4 +1,19 @@
 import java.util.Scanner;
+class SpecificValueFinder{
+    int x[]=new int[5];
+    public boolean findValue(int x[],int y){
+        for (int i = 0; i < x.length; i++) {
+            if (y==x[i]) {
+                System.out.print("The number "+x[i]+" exist in the Array :"+"a["+i+"]");
+                return true;
+                
+            
+            } 
+        }
+        
+        return false;
+    }
+}
 public class Array_Sum {
     public static void main(String[] args) {
         Scanner sc=new Scanner(System.in);
@@ -11,7 +26,10 @@ public class Array_Sum {
         }
        
             System.out.println("The sum of the array is:"+sum);
-        
+         System.out.print("Do you want to see a specific value in the array:");
+         int b=sc.nextInt();
+        SpecificValueFinder s1=new SpecificValueFinder();
+        s1.findValue(a, b);
 
     }
 }
